29295944e418a6ae3e51174255f60aac4f0054ef
razaimam45 <razaimam45@gmail.com>
2023-09-16 03:44:32 +0400

    Raza Comit

--- run.py
+++ run.py
@@ -33,17 +33,16 @@ transform = transforms.Compose(
     ]
 )
 
-
-def get_reference_attn_matp(model, image_folder, block=-1, n_images=2000, device="cuda", attack_type=["FGSM", "PGD"], select_random=False, plot_path = "./plots/"):
+def get_reference_attn_matp(model, image_folder, block=-1, n_images=2000, device="cuda", attack_type=["FGSM", "PGD"], select_random=False, plot_path = "./plots/", eps=0.03, random_state=0):
     # print(f"On Block {block}")
     # all_attns, mean_attns, mean_attn_diff
     all_attns, mean_attns, mean_attn_diff = mean_attns_N_images(image_folder=image_folder, n_images=n_images, 
-                                                                        block=block, model=model, n_random=select_random, device=device, attack_type=attack_type)
+                                                                        block=block, model=model, n_random=select_random, device=device, attack_type=attack_type, eps = eps, random_state=random_state)
     
     for attack_name in attack_type:
         hist_plot(mean_attns['clean'], mean_attns[attack_name], n_images, no_show=True)
         os.makedirs(os.path.join(plot_path, attack_name), exist_ok=True)
-        plt.savefig(os.path.join(plot_path, attack_name, f"mean_attns_{attack_name}_block_{block}_images_{n_images}.png"))
+        plt.savefig(os.path.join(plot_path, attack_name, f"mean_attns_{attack_name}_block_{args.block}_images_{args.num_train_imgs}_eps_{args.eps}_dataset_{args.dataset_class}.png"))
 
     return all_attns, mean_attns, mean_attn_diff
 
@@ -56,6 +55,7 @@ def get_model(model_path, device):
 
 def get_images_attns(model, image_folder, n_imgs=20, block=-1, device="cuda", attack_type=['all'], eps=0.05, plot=False, rand=False, random_state=None):
     image_files = [f for f in os.listdir(image_folder) if f.endswith(".jpg") or f.endswith(".png")]
+    random.seed(random_state)
     image_files = random.sample(image_files, n_imgs)
     all_imgs = []
     all_attns = []
@@ -123,17 +123,19 @@ def classify_image(img_attn_map, mean_attn_clean, mean_attn_adv, method = 'all')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_path', type=str, default='/home/aneeshashmi/xai/vit_base_patch16_224_in21k_test-accuracy_0.96_chest.pth', help='model checkpoint path')
-    parser.add_argument('--device', type=str, default='cuda:3', help='device')
+    parser.add_argument('--model_path', type=str, default='/home/raza.imam/Documents/HC701B/Project/models/vit_base_patch16_224_in21k_test-accuracy_0.96_chest.pth', help='model checkpoint path')
+    parser.add_argument('--device', type=str, default='cuda', help='device')
     parser.add_argument('--attack', type=str, default='all', choices=['FGSM', "PGD", "all"], help='attack type (all for all)')
-    parser.add_argument('--train_path', type=str, default="/home/aneeshashmi/xai/data/real_data/train/", help='path to train data')
-    parser.add_argument("--num_train_imgs", type=int, default=10, help="number of train images to use")
-    parser.add_argument('--test_path', type=str, default="/home/aneeshashmi/xai/data/real_data/test/", help='path to test data')
-    parser.add_argument("--dataset_class", type=str, default="Normal", choices=["tb", "normal"], help="dataset class")
-    parser.add_argument("--block", type=int, default=-1, help="ViT block to take attention from")
-    parser.add_argument("--eps", type=float, default=0.05, help="epsilon for adversarial attacks")
-    parser.add_argument("--force_recompute", action="store_true", help="force recompute mean images")
+    parser.add_argument('--train_path', type=str, default="/home/raza.imam/Documents/HC701B/Project/data/TB_data/training", help='path to train data')
+    parser.add_argument('--test_path', type=str, default="/home/raza.imam/Documents/HC701B/Project/data/TB_data/testing", help='path to test data')
+    parser.add_argument("--num_train_imgs", type=int, default=5000, help="number of train images to use")
     parser.add_argument("--num_test_imgs", type=int, default=500, help="number of test images to use")
+    parser.add_argument("--dataset_class", type=str, default="Tuberculosis", choices=["Tuberculosis", "Normal"], help="dataset class")
+    parser.add_argument("--block", type=int, default=-1, help="ViT block to take attention from")
+    parser.add_argument("--eps", type=float, default=0.02, help="epsilon for adversarial attacks")
+    parser.add_argument("--force_recompute", action="store_true", help="force recompute mean images")
+    parser.add_argument("--random_state", type=int, default=0, help="Random state for experiments (train and test both)")
+    
 
     args = parser.parse_args()
     print("Arguments:")
@@ -156,29 +158,31 @@ if __name__ == "__main__":
         exits = True
         for attack_name in args.attack + ['clean']:
             # mean_attns[attack_name] = mean_attns[attack_name].cpu().numpy()
-            exits *= os.path.exists(os.path.join("./reference", "mean_images", f"mean_attns_{attack_name}_block_{args.block}_images_{args.num_train_imgs}.npy"))
+            exits *= os.path.exists(os.path.join("./reference", "mean_images", f"mean_attns_{attack_name}_block_{args.block}_images_{args.num_train_imgs}_eps_{args.eps}_dataset_{args.dataset_class}.npy"))
 
     if exits:
         mean_attns = {}
         for attack_name in args.attack + ['clean']:
-            mean_attns[attack_name] = np.load(os.path.join("./reference", "mean_images", f"mean_attns_{attack_name}_block_{args.block}_images_{args.num_train_imgs}.npy"))
+            mean_attns[attack_name] = np.load(os.path.join("./reference", "mean_images", f"mean_attns_{attack_name}_block_{args.block}_images_{args.num_train_imgs}_eps_{args.eps}_dataset_{args.dataset_class}.npy"))
         print(f'Loaded mean images from ./reference/mean_images/')
-    
-    all_attns, mean_attns, mean_attn_diff = get_reference_attn_matp(
-        model = model,
-        image_folder = os.path.join(args.train_path, "Normal"),
-        block = args.block,
-        n_images = args.num_train_imgs,
-        device = args.device,
-        attack_type = args.attack,
-        select_random = False,
-    )
+    else:    
+        all_attns, mean_attns, mean_attn_diff = get_reference_attn_matp(
+            model = model,
+            image_folder = os.path.join(args.train_path, args.dataset_class),
+            block = args.block,
+            n_images = args.num_train_imgs,
+            device = args.device,
+            attack_type = args.attack,
+            select_random = False,
+            eps = args.eps,
+            random_state = args.random_state
+        )
     # save mean images to disk
     os.makedirs(os.path.join("./reference", "mean_images"), exist_ok=True)
     print(f'Saving mean images to ./reference/mean_images/')
     for attack_name in args.attack + ['clean']:
         # mean_attns[attack_name] = mean_attns[attack_name].cpu().numpy()
-        np.save(os.path.join("./reference", "mean_images", f"mean_attns_{attack_name}_block_{args.block}_images_{args.num_train_imgs}.npy"), mean_attns[attack_name])
+        np.save(os.path.join("./reference", "mean_images", f"mean_attns_{attack_name}_block_{args.block}_images_{args.num_train_imgs}_eps_{args.eps}_dataset_{args.dataset_class}.npy"), mean_attns[attack_name])
 
 
     # Get test images
@@ -231,7 +235,7 @@ if __name__ == "__main__":
 
     results_df = pd.DataFrame(results_dict)
     os.makedirs("./results", exist_ok=True)
-    results_df.to_csv(f"./results/preds_{args.dataset_class}_block_{args.block}_images_{args.num_train_imgs}_eps_{args.eps}.csv", index=False)
+    results_df.to_csv(f"./results/preds_{attack_name}_block_{args.block}_images_{args.num_train_imgs}_eps_{args.eps}_dataset_{args.dataset_class}.csv", index=False)
 
     # 1 = clean
     # 0 = adversarial
@@ -264,11 +268,11 @@ if __name__ == "__main__":
             print(f"F1 score for {method}: {f1_score(gt_labels_bin, pred_bin)}", file=f)
             print(f'-------------------------------------------------------------------', file=f)
 
-            # print to screen
-            print(f'--------------- {method} ---------------')
-            print(f"Accuracy for {method}: {accuracy_score(gt_labels_bin, pred_bin)}")
-            print(f"F1 score for {method}: {f1_score(gt_labels_bin, pred_bin)}")
-            print(f'-------------------------------------------------------------------')
+            # # print to screen
+            # print(f'--------------- {method} ---------------')
+            # print(f"Accuracy for {method}: {accuracy_score(gt_labels_bin, pred_bin)}")
+            # print(f"F1 score for {method}: {f1_score(gt_labels_bin, pred_bin)}")
+            # print(f'-------------------------------------------------------------------')
 
         print(f'-------------------------------------------------------------------', file=f)
         print(f'', file=f)
--- saliency.py
+++ saliency.py
@@ -34,193 +34,193 @@ import sys
 # import pytorch_grad_cam.metrics.road
 
 
-def reshape_transform(tensor, height=14, width=14):
-    result = tensor[:, 1:, :].reshape(tensor.size(0),
-                                      height, width, tensor.size(2))
-    result = result.transpose(2, 3).transpose(1, 2)
-    return result
+# def reshape_transform(tensor, height=14, width=14):
+#     result = tensor[:, 1:, :].reshape(tensor.size(0),
+#                                       height, width, tensor.size(2))
+#     result = result.transpose(2, 3).transpose(1, 2)
+#     return result
 
-def get_pyt_saliency(input_img, block, head, classifier):
-    target_layers = [classifier.blocks[block].attn]
+# def get_pyt_saliency(input_img, block, head, classifier):
+#     target_layers = [classifier.blocks[block].attn]
 
-    method = "gradcam"
-    # Instantiate the selected method
-    methods = {
-        "gradcam": GradCAM,
-        "scorecam": ScoreCAM,
-        "gradcam++": GradCAMPlusPlus,
-        "ablationcam": AblationCAM,
-        "xgradcam": XGradCAM,
-        "eigencam": EigenCAM,
-        "eigengradcam": EigenGradCAM,
-        "layercam": LayerCAM,
-        # "fullgrad": FullGrad
-    }
-    if method not in methods:
-        raise Exception(f"Method {method} not implemented")
-    if method == "ablationcam":
-        cam = methods[method](model=classifier,
-                            target_layers=target_layers,
-                            use_cuda=True,
-                            reshape_transform=reshape_transform,
-                            ablation_layer=AblationLayerVit())
-    else:
-        cam = methods[method](model=classifier,
-                            target_layers=target_layers,
-                            use_cuda=True,
-                            reshape_transform=reshape_transform)
+#     method = "gradcam"
+#     # Instantiate the selected method
+#     methods = {
+#         "gradcam": GradCAM,
+#         "scorecam": ScoreCAM,
+#         "gradcam++": GradCAMPlusPlus,
+#         "ablationcam": AblationCAM,
+#         "xgradcam": XGradCAM,
+#         "eigencam": EigenCAM,
+#         "eigengradcam": EigenGradCAM,
+#         "layercam": LayerCAM,
+#         # "fullgrad": FullGrad
+#     }
+#     if method not in methods:
+#         raise Exception(f"Method {method} not implemented")
+#     if method == "ablationcam":
+#         cam = methods[method](model=classifier,
+#                             target_layers=target_layers,
+#                             use_cuda=True,
+#                             reshape_transform=reshape_transform,
+#                             ablation_layer=AblationLayerVit())
+#     else:
+#         cam = methods[method](model=classifier,
+#                             target_layers=target_layers,
+#                             use_cuda=True,
+#                             reshape_transform=reshape_transform)
         
-    grayscale_cam = cam(input_tensor=input_img,
-                        targets=None,
-                        eigen_smooth=False,
-                        aug_smooth=False)
-    grayscale_cam = grayscale_cam[0, :]
+#     grayscale_cam = cam(input_tensor=input_img,
+#                         targets=None,
+#                         eigen_smooth=False,
+#                         aug_smooth=False)
+#     grayscale_cam = grayscale_cam[0, :]
     
-    return grayscale_cam
+#     return grayscale_cam
         
 
-def get_attn(input_img, block, head, classifier):   
-    output, attn = classifier(input_img, return_attn=True)
-    normal_attn_map = attn[block][0][head][:-1,-1].reshape(14, -1).detach().cpu().numpy()
-    normal_attn_map = (normal_attn_map -normal_attn_map.min()) / (normal_attn_map.max() -normal_attn_map.min())
-    return normal_attn_map
+# def get_attn(input_img, block, head, classifier):   
+#     output, attn = classifier(input_img, return_attn=True)
+#     normal_attn_map = attn[block][0][head][:-1,-1].reshape(14, -1).detach().cpu().numpy()
+#     normal_attn_map = (normal_attn_map -normal_attn_map.min()) / (normal_attn_map.max() -normal_attn_map.min())
+#     return normal_attn_map
 
-def mean_attn_heads(block, N_heads, input_img, classifier): #N_heads=first N heads of a block
-    # returns mean attention of N_heads for Input image input_image
-    attentions = []
-    for head in range(N_heads):
-        # print(f"For Block {block} head {head}")
-        attn = get_attn(input_img.unsqueeze(0), block, head, classifier)
-        # saliency = (saliency -saliency.min()) / (saliency.max() -saliency.min()) # Normalizing Normal ---
-        attentions.append(attn)
+# def mean_attn_heads(block, N_heads, input_img, classifier): #N_heads=first N heads of a block
+#     # returns mean attention of N_heads for Input image input_image
+#     attentions = []
+#     for head in range(N_heads):
+#         # print(f"For Block {block} head {head}")
+#         attn = get_attn(input_img.unsqueeze(0), block, head, classifier)
+#         # saliency = (saliency -saliency.min()) / (saliency.max() -saliency.min()) # Normalizing Normal ---
+#         attentions.append(attn)
 
-    mean_attn_of_N_heads = np.mean(attentions, axis=0)
-    mean_attn_of_N_heads = (mean_attn_of_N_heads - mean_attn_of_N_heads.min())/(mean_attn_of_N_heads.max()-mean_attn_of_N_heads.min())
+#     mean_attn_of_N_heads = np.mean(attentions, axis=0)
+#     mean_attn_of_N_heads = (mean_attn_of_N_heads - mean_attn_of_N_heads.min())/(mean_attn_of_N_heads.max()-mean_attn_of_N_heads.min())
 
-    return mean_attn_of_N_heads
+#     return mean_attn_of_N_heads
 
-def multiply_attn_heads(block, N_heads, input_img, classifier): #N_heads=first N heads of a block
-    # returns mean attention of N_heads for Input image input_image
-    attentions = []
-    for head in range(N_heads):
-        # print(f"For Block {block} head {head}")
-        attn = get_attn(input_img.unsqueeze(0), block, head, classifier)
-        # saliency = (saliency -saliency.min()) / (saliency.max() -saliency.min()) # Normalizing Normal ---
-        attentions.append(attn)
+# def multiply_attn_heads(block, N_heads, input_img, classifier): #N_heads=first N heads of a block
+#     # returns mean attention of N_heads for Input image input_image
+#     attentions = []
+#     for head in range(N_heads):
+#         # print(f"For Block {block} head {head}")
+#         attn = get_attn(input_img.unsqueeze(0), block, head, classifier)
+#         # saliency = (saliency -saliency.min()) / (saliency.max() -saliency.min()) # Normalizing Normal ---
+#         attentions.append(attn)
 
-    prod_attn = np.ones_like(attentions[0])
-    for attn in attentions:
-        prod_attn *= attn
-    prod_attn = (prod_attn - prod_attn.min())/(prod_attn.max()-prod_attn.min())
-    return prod_attn
+#     prod_attn = np.ones_like(attentions[0])
+#     for attn in attentions:
+#         prod_attn *= attn
+#     prod_attn = (prod_attn - prod_attn.min())/(prod_attn.max()-prod_attn.min())
+#     return prod_attn
 
-def mean_saliency_generate(N_images, image_folder, block, head, classifier, N_random=True, N_heads=1, get_sal = get_attn, agg_attn_heads=mean_attn_heads):
-    # image_folder = '/home/raza.imam/Documents/HC701B/Project/data/TB_data/training/Normal'
+# def mean_saliency_generate(N_images, image_folder, block, head, classifier, N_random=True, N_heads=1, get_sal = get_attn, agg_attn_heads=mean_attn_heads):
+#     # image_folder = '/home/raza.imam/Documents/HC701B/Project/data/TB_data/training/Normal'
 
-    transform = transforms.Compose([
-                transforms.Grayscale(num_output_channels=3),
-                transforms.RandomRotation (20),
-                transforms.Resize((224,224)),
-                transforms.RandomAffine(degrees =0,translate=(0.1,0.1)),
-                transforms.ToTensor()
-    ])
+#     transform = transforms.Compose([
+#                 transforms.Grayscale(num_output_channels=3),
+#                 transforms.RandomRotation (20),
+#                 transforms.Resize((224,224)),
+#                 transforms.RandomAffine(degrees =0,translate=(0.1,0.1)),
+#                 transforms.ToTensor()
+#     ])
 
-    image_files = [f for f in os.listdir(image_folder) if f.endswith('.jpg') or f.endswith('.png')]
-    N, rand = N_images, N_random
-    if rand==False:
-        image_files = image_files[:N]  #first N images
-    else:
-        image_files = random.sample(image_files, N) #random N images
+#     image_files = [f for f in os.listdir(image_folder) if f.endswith('.jpg') or f.endswith('.png')]
+#     N, rand = N_images, N_random
+#     if rand==False:
+#         image_files = image_files[:N]  #first N images
+#     else:
+#         image_files = random.sample(image_files, N) #random N images
 
-    images = []
-    for f in image_files:
-        image_path = os.path.join(image_folder, f)
-        image = Image.open(image_path)
-        image = transform(image)
-        image = (image -image.min()) / (image.max() -image.min())
-        images.append(image)
+#     images = []
+#     for f in image_files:
+#         image_path = os.path.join(image_folder, f)
+#         image = Image.open(image_path)
+#         image = transform(image)
+#         image = (image -image.min()) / (image.max() -image.min())
+#         images.append(image)
 
-    images_tensor = torch.stack(images)
+#     images_tensor = torch.stack(images)
 
-    saliencies_normal = []
-    for input_img in images_tensor:
-        # print(input_img.min(), input_img.max())
-        if N_heads==1: # For 1 particular head
-            saliency = get_sal(input_img.unsqueeze(0), block, head, classifier)
-        else: # For N heads of a block
-            saliency = agg_attn_heads(block, N_heads, input_img, classifier)
-        # saliency = (saliency -saliency.min()) / (saliency.max() -saliency.min()) # Normalizing Normal ---
-        saliencies_normal.append(saliency)
+#     saliencies_normal = []
+#     for input_img in images_tensor:
+#         # print(input_img.min(), input_img.max())
+#         if N_heads==1: # For 1 particular head
+#             saliency = get_sal(input_img.unsqueeze(0), block, head, classifier)
+#         else: # For N heads of a block
+#             saliency = agg_attn_heads(block, N_heads, input_img, classifier)
+#         # saliency = (saliency -saliency.min()) / (saliency.max() -saliency.min()) # Normalizing Normal ---
+#         saliencies_normal.append(saliency)
 
-    mean_saliency_normal = np.mean(saliencies_normal, axis=0)
-    print("mean_saliency_normal.shape:", mean_saliency_normal.shape)
+#     mean_saliency_normal = np.mean(saliencies_normal, axis=0)
+#     print("mean_saliency_normal.shape:", mean_saliency_normal.shape)
 
-    pgd = PGD(classifier, lower_bound=0, upper_bound=1)
-    adv_images = []
-    for input_img in images_tensor:
-        input_img = input_img.unsqueeze(0).float()
-        perturbed_image = pgd.perturb(input_img, radius=0.13, step_size=0.02, step_num=7, target=0) 
-        adv_sample = torch.tensor((perturbed_image.cpu().data.numpy()))
-        adv_images.append(adv_sample.squeeze(0))
+#     pgd = PGD(classifier, lower_bound=0, upper_bound=1)
+#     adv_images = []
+#     for input_img in images_tensor:
+#         input_img = input_img.unsqueeze(0).float()
+#         perturbed_image = pgd.perturb(input_img, radius=0.13, step_size=0.02, step_num=7, target=0) 
+#         adv_sample = torch.tensor((perturbed_image.cpu().data.numpy()))
+#         adv_images.append(adv_sample.squeeze(0))
 
-    adv_images_tensor = torch.stack(adv_images)
+#     adv_images_tensor = torch.stack(adv_images)
 
-    saliencies_adv = []
-    for adv_img in adv_images_tensor:
-        if N_heads==1: # For 1 particular head
-            saliency = get_sal(adv_img.unsqueeze(0), block, head, classifier)
-        else: # For N heads of a block
-            saliency = agg_attn_heads(block, N_heads, adv_img, classifier)
-        # saliency = (saliency -saliency.min()) / (saliency.max() -saliency.min()) # Normalizing Adv ---
-        saliencies_adv.append(saliency)
+#     saliencies_adv = []
+#     for adv_img in adv_images_tensor:
+#         if N_heads==1: # For 1 particular head
+#             saliency = get_sal(adv_img.unsqueeze(0), block, head, classifier)
+#         else: # For N heads of a block
+#             saliency = agg_attn_heads(block, N_heads, adv_img, classifier)
+#         # saliency = (saliency -saliency.min()) / (saliency.max() -saliency.min()) # Normalizing Adv ---
+#         saliencies_adv.append(saliency)
 
-    mean_saliency_adv = np.mean(saliencies_adv, axis=0)
-    print("mean_saliency_adv.shape:", mean_saliency_adv.shape)    
+#     mean_saliency_adv = np.mean(saliencies_adv, axis=0)
+#     print("mean_saliency_adv.shape:", mean_saliency_adv.shape)    
 
-    saliency_diff = mean_saliency_adv - mean_saliency_normal
-    print("saliency_diff.shape:", saliency_diff.shape)    
+#     saliency_diff = mean_saliency_adv - mean_saliency_normal
+#     print("saliency_diff.shape:", saliency_diff.shape)    
 
-    print(mean_saliency_normal.shape)
-    return mean_saliency_normal, mean_saliency_adv, saliency_diff, saliencies_normal, saliencies_adv
+#     print(mean_saliency_normal.shape)
+#     return mean_saliency_normal, mean_saliency_adv, saliency_diff, saliencies_normal, saliencies_adv
 
 
-def test_img_saliency(image_folder, block, head, classifier, plot=False, rand=True):
-    if rand==True:
-        f_0 = random.choice(os.listdir(image_folder))
-    else:
-        f_0 = os.listdir(image_folder)[0]
-    image_path = os.path.join(image_folder, f_0)
-    transform = transforms.Compose([
-                transforms.Grayscale(num_output_channels=3),
-                transforms.RandomRotation (20),
-                transforms.Resize((224,224)),
-                transforms.RandomAffine(degrees =0,translate=(0.1,0.1)),
-                transforms.ToTensor()
-    ])
-    image_0 = transform(Image.open(image_path))
-    image_0 = (image_0 -image_0.min()) / (image_0.max() -image_0.min())
-    image_tensor_0 = image_0.unsqueeze(0)
-    # print(image_tensor_0.min(), image_tensor_0.max())
+# def test_img_saliency(image_folder, block, head, classifier, plot=False, rand=True):
+#     if rand==True:
+#         f_0 = random.choice(os.listdir(image_folder))
+#     else:
+#         f_0 = os.listdir(image_folder)[0]
+#     image_path = os.path.join(image_folder, f_0)
+#     transform = transforms.Compose([
+#                 transforms.Grayscale(num_output_channels=3),
+#                 transforms.RandomRotation (20),
+#                 transforms.Resize((224,224)),
+#                 transforms.RandomAffine(degrees =0,translate=(0.1,0.1)),
+#                 transforms.ToTensor()
+#     ])
+#     image_0 = transform(Image.open(image_path))
+#     image_0 = (image_0 -image_0.min()) / (image_0.max() -image_0.min())
+#     image_tensor_0 = image_0.unsqueeze(0)
+#     # print(image_tensor_0.min(), image_tensor_0.max())
 
-    saliency_0 = get_attn(image_tensor_0, block, head, classifier)
-    saliency_normal_0 = saliency_0
-    # saliency_normal_0 = (saliency_0 -saliency_0.min()) / (saliency_0.max() -saliency_0.min()) # Normalizing ...
+#     saliency_0 = get_attn(image_tensor_0, block, head, classifier)
+#     saliency_normal_0 = saliency_0
+#     # saliency_normal_0 = (saliency_0 -saliency_0.min()) / (saliency_0.max() -saliency_0.min()) # Normalizing ...
 
-    pgd_0 = PGD(classifier, lower_bound=0, upper_bound=1)
-    input_img_0 = image_tensor_0.float()
-    perturbed_image_0 = pgd_0.perturb(input_img_0, radius=0.13, step_size=0.02, step_num=7, target=0)
-    adv_sample_0 = perturbed_image_0.cpu().data.numpy()
-    adv_image_tensor_0 = torch.tensor(adv_sample_0)
+#     pgd_0 = PGD(classifier, lower_bound=0, upper_bound=1)
+#     input_img_0 = image_tensor_0.float()
+#     perturbed_image_0 = pgd_0.perturb(input_img_0, radius=0.13, step_size=0.02, step_num=7, target=0)
+#     adv_sample_0 = perturbed_image_0.cpu().data.numpy()
+#     adv_image_tensor_0 = torch.tensor(adv_sample_0)
 
-    adv_saliency_0 = get_attn(adv_image_tensor_0, block, head, classifier)
-    saliency_adv_0 = adv_saliency_0
+#     adv_saliency_0 = get_attn(adv_image_tensor_0, block, head, classifier)
+#     saliency_adv_0 = adv_saliency_0
 
-    saliency_diff = saliency_adv_0 - saliency_normal_0
+#     saliency_diff = saliency_adv_0 - saliency_normal_0
     
-    if plot==True:
-        plot_sal_hist_kde(saliency_normal_0, saliency_adv_0, saliency_diff, 1, kde=False, pca=False)
+#     if plot==True:
+#         plot_sal_hist_kde(saliency_normal_0, saliency_adv_0, saliency_diff, 1, kde=False, pca=False)
     
-    return f_0, saliency_normal_0, saliency_adv_0, saliency_diff
+#     return f_0, saliency_normal_0, saliency_adv_0, saliency_diff
 
 
 
@@ -273,6 +273,7 @@ def mean_attns_N_images(
         N_heads=12, 
         attack_type="PGD", 
         eps=0.05, 
+        random_state=0,
         transform = None
         ):
 
@@ -292,6 +293,7 @@ def mean_attns_N_images(
     if n_random==False:
         image_files = image_files[:n_images]  #first N images
     else:
+        random.seed(random_state)
         image_files = random.sample(image_files, n_images) #random N images
 
     #REVIEW:creating list of N clean images
