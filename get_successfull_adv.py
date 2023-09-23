9b869f94ba18586ec4e489a0f1f8cd70345c01b4
razaimam45 <razaimam45@gmail.com>
2023-09-23 07:10:08 +0400

    Successful samples analyzed PPT

--- get_successfull_adv.py
+++ get_successfull_adv.py
@@ -60,10 +60,9 @@ def get_blk_attn(input_img, blk, model, patch_size=16): #REVIEW:function to get 
     attentions = nn.functional.interpolate(attentions.unsqueeze(
             0), scale_factor=patch_size, mode="nearest")[0].cpu().numpy()
     attentions = attentions.transpose(0,2,1)
-    # print(attentions.shape) #REVIEW:list of attentions from each head, after interpolation. Shape = torch.Size([1, 12, 224, 224])
     mean_attention = np.mean(attentions, 0)
 
-    return mean_attention, torch.argmax(out).item() #REVIEW:Return mean of 12 head attentions
+    return mean_attention, torch.argmax(out).item()
 
 def folder_sort(image_files):
     image_files = sorted(image_files, key=lambda x: int(x.split('.')[0]))
@@ -79,9 +78,9 @@ model = get_model(model_path=model_path, device=device)
 
 
 normal_testing = [f for f in os.listdir(os.path.join(data_path, 'testing', classes[0])) if f.endswith(".jpg") or f.endswith(".png")]
-normal_testing = (normal_testing)[0:10]
+normal_testing = (normal_testing)[0:]
 tb_testing = [f for f in os.listdir(os.path.join(data_path, 'testing', classes[1])) if f.endswith(".jpg") or f.endswith(".png")]
-tb_testing = (tb_testing)[0:10]
+tb_testing = (tb_testing)[0:]
 
 blk = -1
 
@@ -138,54 +137,53 @@ for img_path in tqdm(tb_testing):
     image_test_tb.append(img)
 print('loading done')
 
-def get_success_unsuccess_adv_images (images_list, cls_idx):
+def get_success_unsuccess_adv_images (adv_images_list, cls_idx, clean_images):
     success_imgs = []
     unsuccess_imgs = []
     success_imgs_attns = []
     unsuccess_imgs_attns = []
+    
+    clean_succ_x = []
+    clean_unsucc_N_x = []
+    clean_succ_x_attns = []
+    clean_unsucc_N_x_attns = []
 
-    for img in tqdm(images_list):
+    for i, img in tqdm(enumerate(adv_images_list)):
         attn, pred = get_blk_attn(img.unsqueeze(0).to(device), blk, model)
         if pred != cls_idx: # check if pred is not correct
             success_imgs.append(img.permute(1,2,0)) #append to successful adv samples
             success_imgs_attns.append(attn)
+            clean_succ_x.append(clean_images[i])
+            clean_succ_x_attns.append(get_blk_attn(clean_images[i].to(device), blk, model))
+            
         else:
             unsuccess_imgs.append(img.permute(1,2,0)) #append to UNsuccessful adv samples
             unsuccess_imgs_attns.append(attn)
-    return success_imgs, success_imgs_attns, unsuccess_imgs, unsuccess_imgs_attns
+            clean_unsucc_N_x.append(clean_images[i])
+            clean_unsucc_N_x_attns.append(get_blk_attn(clean_images[i].to(device), blk, model))
+            
+            
+    return success_imgs, success_imgs_attns, unsuccess_imgs, unsuccess_imgs_attns, clean_succ_x, clean_unsucc_N_x,  clean_succ_x_attns, clean_unsucc_N_x_attns 
 
 # -------------------------------------- # 
-# -------------------------------------- #
-# -------------------------------------- #
 
 
-# # argparse
-# parser = argparse.ArgumentParser(description='Get successful adversarial samples')
-# parser.add_argument('--eps', type=float, default=0.03, help='epsilon for PGD')
-# args = parser.parse_args()
-# eps = args.eps
 
-# print("----------------------------------------------")
-# print("----------------------------------------------")
-# # print(f'Running for Epsilon = {eps}')
-# print("----------------------------------------------")
-# print("----------------------------------------------")
-
-#Saving clean test Normal
-save_folder_cleans = f"./data2/clean/Test/Normal"
-print(f'Saving {len(image_test_normal)} cleans to {save_folder_cleans}')
-os.makedirs(save_folder_cleans, exist_ok=True)
-for i, img in tqdm(enumerate(image_test_normal)):
-    img = img.squeeze(0).squeeze(0).permute(1, 2 ,0).to('cpu')
-    plt.imsave(os.path.join(save_folder_cleans, f"{i}.png"), np.array(img))
+# #Saving clean test Normal
+# save_folder_cleans = f"./data2/clean/Test/Normal"
+# print(f'Saving {len(image_test_normal)} cleans to {save_folder_cleans}')
+# os.makedirs(save_folder_cleans, exist_ok=True)
+# for i, img in tqdm(enumerate(image_test_normal)):
+#     img = img.squeeze(0).squeeze(0).permute(1, 2 ,0).to('cpu')
+#     plt.imsave(os.path.join(save_folder_cleans, f"{i}.png"), np.array(img))
     
-#Saving clean test TB
-save_folder_cleans = f"./data2/clean/Test/TB"
-print(f'Saving {len(image_test_tb)} cleans to {save_folder_cleans}')
-os.makedirs(save_folder_cleans, exist_ok=True)
-for i, img in tqdm(enumerate(image_test_tb)):
-    img = img.squeeze(0).squeeze(0).permute(1, 2, 0).to('cpu')
-    plt.imsave(os.path.join(save_folder_cleans, f"{i}.png"), np.array(img))
+# #Saving clean test TB
+# save_folder_cleans = f"./data2/clean/Test/TB"
+# print(f'Saving {len(image_test_tb)} cleans to {save_folder_cleans}')
+# os.makedirs(save_folder_cleans, exist_ok=True)
+# for i, img in tqdm(enumerate(image_test_tb)):
+#     img = img.squeeze(0).squeeze(0).permute(1, 2, 0).to('cpu')
+#     plt.imsave(os.path.join(save_folder_cleans, f"{i}.png"), np.array(img))
 
 
 # %%
@@ -197,6 +195,7 @@ f_fgsm = fb.attacks.FGSM()
 
 for eps in [0.03, 0.06, 0.01]:
     for attack in ['PDG', 'FGSM']:
+        class_name = 'Normal'      
         pred_i = 0 # 0 --> Normal, 1 --> TB
         print(classes[pred_i])
         print(f'Running {attack} for Epsilon = {eps} on Test normal')
@@ -204,28 +203,45 @@ for eps in [0.03, 0.06, 0.01]:
             adv_images_test_normal = apply_pdg(images=  image_test_normal, f_model=f_model, f_pgd=f_pgd, labels=torch.tensor([pred_i]), eps = eps)
         else:
             adv_images_test_normal = apply_fgsm(images=  image_test_normal, f_model=f_model, f_fgsm = f_fgsm, labels=torch.tensor([pred_i]), device=device, eps=eps)
-        succ_imgs, succ_attns, unsucc_imgs, unsucc_attns = get_success_unsuccess_adv_images(adv_images_test_normal, pred_i)
-        save_folder_succ_imgs = f"./data2/successful_{eps}/{attack}/Test/Normal"
-        save_folder_succ_attns = f"./data2/successful_attns_{eps}/{attack}/Test/Normal"
-        save_folder_unsucc_imgs = f"./data2/unsuccessful_{eps}/{attack}/Test/Normal"
-        save_folder_unsucc_attns = f"./data2/unsuccessful_attns_{eps}/{attack}/Test/Normal"
-        print(f'Saving {len(succ_imgs)} images to {save_folder_succ_imgs}')
-        print(f'Saving {len(unsucc_imgs)} images to {save_folder_unsucc_imgs}')
-        os.makedirs(save_folder_succ_imgs, exist_ok=True)
-        os.makedirs(save_folder_succ_attns, exist_ok=True)
-        os.makedirs(save_folder_unsucc_imgs, exist_ok=True)
-        os.makedirs(save_folder_unsucc_attns, exist_ok=True)
-        for i, img in tqdm(enumerate(succ_imgs)):
-            plt.imsave(os.path.join(save_folder_succ_imgs, f"{i}.png"), np.array(img))
-            np.save(os.path.join(save_folder_succ_attns, f"{i}.npy"), succ_attns[i])
-        for i, img in tqdm(enumerate(unsucc_imgs)):
-            plt.imsave(os.path.join(save_folder_unsucc_imgs, f"{i}.png"), np.array(img))
-            np.save(os.path.join(save_folder_unsucc_attns, f"{i}.npy"), unsucc_attns[i])
+        succ_imgs, succ_attns, unsucc_imgs, unsucc_attns, clean_succ_x, clean_unsucc_N_x, clean_succ_x_attns, clean_unsucc_N_x_attns = get_success_unsuccess_adv_images(adv_images_test_normal, pred_i, image_test_normal)
+        
+        save_dir_succ_x = f"./data3/{attack}_{eps}/{class_name}/Succ/Succ_x/"
+        save_dir_succ_clean_x = f"./data3/{attack}_{eps}/{class_name}/Succ/Clean_x/"
+        save_dir_unsucc_N_x = f"./data3/{attack}_{eps}/{class_name}/Unsucc/Unsucc_x/"
+        save_dir_unsucc_clean_N_x = f"./data3/{attack}_{eps}/{class_name}/Unsucc/Clean_x/"
+        
+        save_dir_succ_x_attn = f"./data3/{attack}_{eps}/{class_name}/Succ/Succ_x_attn/"
+        save_dir_succ_clean_x_attn = f"./data3/{attack}_{eps}/{class_name}/Succ/Clean_x_attn/"
+        save_dir_unsucc_N_x_attn = f"./data3/{attack}_{eps}/{class_name}/Unsucc/Unsucc_x_attn/"
+        save_dir_unsucc_clean_N_x_attn = f"./data3/{attack}_{eps}/{class_name}/Unsucc/Clean_x_attn/"
+        
+        os.makedirs(save_dir_succ_x, exist_ok=True)
+        os.makedirs(save_dir_succ_clean_x, exist_ok=True)
+        os.makedirs(save_dir_unsucc_N_x, exist_ok=True)
+        os.makedirs(save_dir_unsucc_clean_N_x, exist_ok=True)  
+        
+        os.makedirs(save_dir_succ_x_attn, exist_ok=True)
+        os.makedirs(save_dir_succ_clean_x_attn, exist_ok=True)
+        os.makedirs(save_dir_unsucc_N_x_attn, exist_ok=True)
+        os.makedirs(save_dir_unsucc_clean_N_x_attn, exist_ok=True)  
+                
+        for i, _ in tqdm(enumerate(succ_imgs)):
+            plt.imsave(os.path.join(save_dir_succ_x, f"{i}.png"), np.array(succ_imgs[i]))
+            np.save(os.path.join(save_dir_succ_x_attn, f"{i}.npy"), succ_attns[i])
+            plt.imsave(os.path.join(save_dir_succ_clean_x, f"{i}.png"), np.array(clean_succ_x[i].squeeze(0).permute(1,2,0).to('cpu')))
+            np.save(os.path.join(save_dir_succ_clean_x_attn, f"{i}.npy"), clean_succ_x_attns[i])
+            
+        for i, _ in tqdm(enumerate(unsucc_imgs)):
+            plt.imsave(os.path.join(save_dir_unsucc_N_x, f"{i}.png"), np.array(unsucc_imgs[i]))
+            np.save(os.path.join(save_dir_unsucc_N_x_attn, f"{i}.npy"), unsucc_attns[i])
+            plt.imsave(os.path.join(save_dir_unsucc_clean_N_x, f"{i}.png"), np.array(clean_unsucc_N_x[i].squeeze(0).permute(1,2,0).to('cpu')))
+            np.save(os.path.join(save_dir_unsucc_clean_N_x_attn, f"{i}.npy"), clean_unsucc_N_x_attns[i])
 
         print("----------------------------------------------")
         print("----------------------------------------------")
         
         # for TB class
+        class_name = 'TB'      
         pred_i = 1 # 0 --> Normal, 1 --> TB
         print(classes[pred_i])
         print(f'Running {attack} for Epsilon = {eps} on Test TB')
@@ -233,21 +249,36 @@ for eps in [0.03, 0.06, 0.01]:
             adv_images_test_tb = apply_pdg(images=  image_test_tb, f_model=f_model, f_pgd=f_pgd, labels=torch.tensor([pred_i]), eps = eps)
         else:
             adv_images_test_tb = apply_fgsm(images=  image_test_tb, f_model=f_model, f_fgsm = f_fgsm, labels=torch.tensor([pred_i]), device=device, eps=eps)
-        succ_imgs, succ_attns, unsucc_imgs, unsucc_attns = get_success_unsuccess_adv_images(adv_images_test_tb, pred_i)
+        succ_imgs, succ_attns, unsucc_imgs, unsucc_attns, clean_succ_x, clean_unsucc_N_x, clean_succ_x_attns, clean_unsucc_N_x_attns = get_success_unsuccess_adv_images(adv_images_test_tb, pred_i, image_test_tb)
         
-        save_folder_succ_imgs = f"./data2/successful_{eps}/{attack}/Test/TB"
-        save_folder_succ_attns = f"./data2/successful_attns_{eps}/{attack}/Test/TB"
-        save_folder_unsucc_imgs = f"./data2/unsuccessful_{eps}/{attack}/Test/TB"
-        save_folder_unsucc_attns = f"./data2/unsuccessful_attns_{eps}/{attack}/Test/TB"
-        print(f'Saving {len(succ_imgs)} images to {save_folder_succ_imgs}')
-        print(f'Saving {len(unsucc_imgs)} images to {save_folder_unsucc_imgs}')
-        os.makedirs(save_folder_succ_imgs, exist_ok=True)
-        os.makedirs(save_folder_succ_attns, exist_ok=True)
-        os.makedirs(save_folder_unsucc_imgs, exist_ok=True)
-        os.makedirs(save_folder_unsucc_attns, exist_ok=True)
-        for i, img in tqdm(enumerate(succ_imgs)):
-            plt.imsave(os.path.join(save_folder_succ_imgs, f"{i}.png"), np.array(img))
-            np.save(os.path.join(save_folder_succ_attns, f"{i}.npy"), succ_attns[i])
-        for i, img in tqdm(enumerate(unsucc_imgs)):
-            plt.imsave(os.path.join(save_folder_unsucc_imgs, f"{i}.png"), np.array(img))
-            np.save(os.path.join(save_folder_unsucc_attns, f"{i}.npy"), unsucc_attns[i])
+        save_dir_succ_x = f"./data3/{attack}_{eps}/{class_name}/Succ/Succ_x/"
+        save_dir_succ_clean_x = f"./data3/{attack}_{eps}/{class_name}/Succ/Clean_x/"
+        save_dir_unsucc_N_x = f"./data3/{attack}_{eps}/{class_name}/Unsucc/Unsucc_x/"
+        save_dir_unsucc_clean_N_x = f"./data3/{attack}_{eps}/{class_name}/Unsucc/Clean_x/"
+        
+        save_dir_succ_x_attn = f"./data3/{attack}_{eps}/{class_name}/Succ/Succ_x_attn/"
+        save_dir_succ_clean_x_attn = f"./data3/{attack}_{eps}/{class_name}/Succ/Clean_x_attn/"
+        save_dir_unsucc_N_x_attn = f"./data3/{attack}_{eps}/{class_name}/Unsucc/Unsucc_x_attn/"
+        save_dir_unsucc_clean_N_x_attn = f"./data3/{attack}_{eps}/{class_name}/Unsucc/Clean_x_attn/"
+        
+        os.makedirs(save_dir_succ_x, exist_ok=True)
+        os.makedirs(save_dir_succ_clean_x, exist_ok=True)
+        os.makedirs(save_dir_unsucc_N_x, exist_ok=True)
+        os.makedirs(save_dir_unsucc_clean_N_x, exist_ok=True)  
+        
+        os.makedirs(save_dir_succ_x_attn, exist_ok=True)
+        os.makedirs(save_dir_succ_clean_x_attn, exist_ok=True)
+        os.makedirs(save_dir_unsucc_N_x_attn, exist_ok=True)
+        os.makedirs(save_dir_unsucc_clean_N_x_attn, exist_ok=True)  
+                
+        for i, _ in tqdm(enumerate(succ_imgs)):
+            plt.imsave(os.path.join(save_dir_succ_x, f"{i}.png"), np.array(succ_imgs[i]))
+            np.save(os.path.join(save_dir_succ_x_attn, f"{i}.npy"), succ_attns[i])
+            plt.imsave(os.path.join(save_dir_succ_clean_x, f"{i}.png"), np.array(clean_succ_x[i].squeeze(0).permute(1,2,0).to('cpu')))
+            np.save(os.path.join(save_dir_succ_clean_x_attn, f"{i}.npy"), clean_succ_x_attns[i])
+            
+        for i, _ in tqdm(enumerate(unsucc_imgs)):
+            plt.imsave(os.path.join(save_dir_unsucc_N_x, f"{i}.png"), np.array(unsucc_imgs[i]))
+            np.save(os.path.join(save_dir_unsucc_N_x_attn, f"{i}.npy"), unsucc_attns[i])
+            plt.imsave(os.path.join(save_dir_unsucc_clean_N_x, f"{i}.png"), np.array(clean_unsucc_N_x[i].squeeze(0).permute(1,2,0).to('cpu')))
+            np.save(os.path.join(save_dir_unsucc_clean_N_x_attn, f"{i}.npy"), clean_unsucc_N_x_attns[i])
--- test_raza_ppt2.py
+++ test_raza_ppt2.py
@@ -4,15 +4,11 @@ from saliency import get_blk_attn
 from utils import *
 from plots import *
 from run import *
-import foolbox as fb
 
-"""
+# """
 # image_folder = "/home/raza.imam/Documents/HC701B/Project/adv_data/TB_adversarial_data/testing/Tuberculosis"
 def return_a_img(image_folder, img_num=0):
-    
-    image_files = [
-            f for f in os.listdir(image_folder) if f.endswith(f".png")
-        ]
+    image_files = [f for f in os.listdir(image_folder) if f.endswith(f".png")]
     image_files = sorted(image_files, key=lambda x: int(x.split('.')[0]))
     print((image_files[0]))
 
@@ -20,88 +16,6 @@ def return_a_img(image_folder, img_num=0):
         [
             transforms.Grayscale(num_output_channels=3),
             transforms.RandomRotation((90,90)),
-            # transforms.CenterCrop(400),
-            transforms.Resize((224, 224)),
-            # transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
-            transforms.ToTensor(),
-        ]
-    )
-
-    image_path = os.path.join(image_folder, image_files[img_num])
-    image = Image.open(image_path)
-    img = transform(image)
-    img = img.unsqueeze(0)
-
-    return img
-
-device = "cuda"
-class_name = 'Normal'
-epsilon = 0.03
-img_num = 0
-
-print(f"For '{class_name}', 'eps={epsilon}':--> Successful PGD+FGSM V/S Unsuccessful PGD+FGSM")
-
-# model = vit_base_patch16_224(pretrained=False)
-model = torch.load('/home/raza.imam/Documents/HC701B/Project/models/vit_base_patch16_224_in21k_test-accuracy_0.96_chest.pth')
-model = model.to(device)
-
-cln_img_folder = f"/home/raza.imam/Documents/XAIM/XAIM/data/clean/Test/{class_name}"
-suc_pgd_img_folder = f"/home/raza.imam/Documents/XAIM/XAIM/data/successful_{epsilon}/PDG/Test/{class_name}"
-suc_fgsm_img_folder = f"/home/raza.imam/Documents/XAIM/XAIM/data/successful_{epsilon}/FGSM/Test/{class_name}"
-uns_pgd_img_folder = f"/home/raza.imam/Documents/XAIM/XAIM/data/unsuccessful_{epsilon}/PDG/Test/{class_name}"
-uns_fgsm_img_folder = f"/home/raza.imam/Documents/XAIM/XAIM/data/unsuccessful_{epsilon}/FGSM/Test/{class_name}"
-
-cln_img = return_a_img(cln_img_folder, img_num=img_num)
-suc_pgd_img = return_a_img(suc_pgd_img_folder, img_num=img_num)
-suc_fgsm_img = return_a_img(suc_fgsm_img_folder, img_num=img_num)
-uns_pgd_img = return_a_img(uns_pgd_img_folder, img_num=img_num)
-uns_fgsm_img = return_a_img(uns_fgsm_img_folder, img_num=img_num)
-
-block = -1
-cln_img_attn = get_blk_attn(input_img=cln_img.cuda(), blk=block, model=model)
-suc_pgd_img_attn = get_blk_attn(input_img=suc_pgd_img.cuda(), blk=block, model=model)
-suc_fgsm_img_attn = get_blk_attn(input_img=suc_fgsm_img.cuda(), blk=block, model=model)
-uns_pgd_img_attn = get_blk_attn(input_img=uns_pgd_img.cuda(), blk=block, model=model)
-uns_fgsm_img_attn = get_blk_attn(input_img=uns_fgsm_img.cuda(), blk=block, model=model)
-
-# pgd_img.squeeze(0).permute(2,1,0).cpu()
-
-plt.figure(figsize=(10, 10))
-text = ["Clean Image", "Successful PGD", "Unuccessful PGD", "Attn Clean", "Attn Suc PGD", "Attn Uns PGD"]
-for i, fig in enumerate([cln_img.squeeze(0).permute(2,1,0).cpu(), suc_pgd_img.squeeze(0).permute(2,1,0).cpu(), uns_pgd_img.squeeze(0).permute(2,1,0).cpu(), cln_img_attn, suc_pgd_img_attn, uns_pgd_img_attn]):
-    print(fig.shape)
-    plt.subplot(1, 6, i+1)
-    plt.imshow(fig, cmap='inferno')
-    plt.title(text[i])
-# plt.title(f"For '{class_name}', 'eps={epsilon}'")
-plt.show()
-"""
-
-# """
-# image_folder = "/home/raza.imam/Documents/HC701B/Project/adv_data/TB_adversarial_data/testing/Tuberculosis"
-import os
-import torch
-import numpy as np
-import matplotlib.pyplot as plt
-from torchvision import transforms
-from PIL import Image
-from captum.attr import LayerActivation, LayerGradientXActivation
-
-model_path = '/home/raza.imam/Documents/HC701B/Project/models/vit_base_patch16_224_in21k_test-accuracy_0.96_chest.pth'
-
-model = torch.load(model_path)
-model = model.to('cuda')
-
-def return_a_img(image_folder, img_num=0):
-    image_files = [
-        f for f in os.listdir(image_folder) if f.endswith(".png")
-    ]
-    image_files = sorted(image_files, key=lambda x: int(x.split('.')[0]))
-
-    transform = transforms.Compose(
-        [
-            transforms.Grayscale(num_output_channels=3),
-            # transforms.RandomRotation((90,90)),
             transforms.CenterCrop(200),
             transforms.Resize((224, 224)),
             # transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
@@ -113,37 +27,99 @@ def return_a_img(image_folder, img_num=0):
     image = Image.open(image_path)
     img = transform(image)
     img = img.unsqueeze(0)
-
     return img
 
+def return_npy_as_img(attns_folder, img_num=0):
+    attn_files = [f for f in os.listdir(attns_folder) if f.endswith(f".npy")]
+    attn_files = sorted(attn_files, key=lambda x: int(x.split('.')[0]))
+    attn_path = os.path.join(attns_folder, attn_files[img_num])
+    attn = np.load(attn_path, allow_pickle=True)
+    return attn
+"""
 device = "cuda"
-class_name = 'Normal'
+class_name = 'Normal' #Normal or TB class
+eps = 0.03 #0.01, 0.03, 0.06
+img_num =0
+attack = 'PDG' #PDG or FGSM
+succ_unsucc = 'Succ' #Check Succ or Unsucc samples
 
-eps_list = [0.01, 0.03, 0.06]
-img_num = 6
-blocks = 12
+print(f"For '{class_name}', 'eps={eps}':--> Successful PGD+FGSM V/S Unsuccessful PGD+FGSM")
 
-plt.figure(figsize=(24, 8))
+# model = vit_base_patch16_224(pretrained=False)
+model = torch.load('/home/raza.imam/Documents/HC701B/Project/models/vit_base_patch16_224_in21k_test-accuracy_0.96_chest.pth')
+model = model.to(device)
 
-for eps_idx, epsilon in enumerate(eps_list):
-    img_folder = f"/home/raza.imam/Documents/XAIM/XAIM/data/unsuccessful_{epsilon}/PDG/Test/{class_name}"
-    img = return_a_img(img_folder, img_num=img_num)
-    
-    attentions = []
+#Imgs
+succ_adv_folder = f"./data3/{attack}_{eps}/{class_name}/{succ_unsucc}/{succ_unsucc}_x/"
+clean_folder = f"./data3/{attack}_{eps}/{class_name}/{succ_unsucc}/Clean_x/"
+#Attns
+succ_adv_folder_attn = f"./data3/{attack}_{eps}/{class_name}/{succ_unsucc}/{succ_unsucc}_x_attn/"
+clean_folder_attn = f"./data3/{attack}_{eps}/{class_name}/{succ_unsucc}/Clean_x_attn/"
 
-    for block in range(0, blocks):
-        img_attn = get_blk_attn(input_img=img.cuda(), blk=block, model=model)
-        attentions.append(img_attn.transpose(1,0))
+adv_img = return_a_img(succ_adv_folder, img_num=img_num)
+cln_img = return_a_img(clean_folder, img_num=img_num)
+# adv_attn = return_npy_as_img(attns_folder=succ_adv_folder_attn, img_num=img_num)
+# cln_attn = return_npy_as_img(attns_folder=clean_folder_attn, img_num=img_num)
 
-    plt.subplot(3, 12, eps_idx + 1)
-    plt.imshow(attentions[0], cmap='inferno')
-    # plt.title(f'Epsilon={epsilon}, Block 1')
-    
-    for block in range(0, blocks):
-        plt.subplot(3, 12, eps_idx * blocks + block + 1)
-        plt.imshow(attentions[block], cmap='inferno')
-        plt.title(f'B{block + 1}, E:{epsilon}')
+adv_attn = get_blk_attn(input_img=adv_img.cuda(), blk=-1, model=model)
+cln_attn = get_blk_attn(input_img=cln_img.cuda(), blk=-1, model=model)
 
-plt.tight_layout()
+# pgd_img.squeeze(0).permute(2,1,0).cpu()
+
+plt.figure(figsize=(10, 10))
+text = ["Clean Image", "Successful Adv", "Attn Clean", "Attn Suc Adv"]
+for i, fig in enumerate([cln_img.squeeze(0).permute(2,1,0).cpu(), adv_img.squeeze(0).permute(2,1,0).cpu(), cln_attn, adv_attn]):
+    print(fig.shape)
+    plt.subplot(1, 4, i+1)
+    plt.imshow(fig, cmap='inferno')
+    plt.title(text[i])
+# plt.title(f"For '{class_name}', 'eps={epsilon}'")
 plt.show()
-# """
+"""
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+device = "cuda"
+class_name = 'Normal'  # Normal or TB class
+epsilons = [0.01, 0.03, 0.06]
+attack = 'PDG'  # PDG or FGSM
+img_num = 0
+succ_unsucc = 'Succ' #Check Succ or Unsucc samples
+
+model = torch.load('/home/raza.imam/Documents/HC701B/Project/models/vit_base_patch16_224_in21k_test-accuracy_0.96_chest.pth')
+model = model.to(device)
+
+def get_attention_maps(input_img, model):
+    attention_maps = []
+    for blk in range(12):
+        attn = get_blk_attn(input_img=input_img, blk=blk, model=model)
+        attention_maps.append(attn)
+    return attention_maps
+
+fig, axes = plt.subplots(4, 13, figsize=(48, 24))  # Use 4 rows
+clean_folder = f"./data3/{attack}_{epsilons[0]}/{class_name}/{succ_unsucc}/Clean_x/"
+cln_img = return_a_img(clean_folder, img_num=img_num)
+axes[3, 0].imshow(np.transpose(cln_img.squeeze().cpu().numpy(), (2, 1, 0)))  # Use the 4th row
+
+for blk in range(12):
+    cln_attn_maps = get_attention_maps(input_img=cln_img.cuda(), model=model)
+    axes[3, blk+1].imshow(cln_attn_maps[blk], cmap='inferno')  # Use the 4th row
+    axes[3, blk+1].set_title(f'Cln B:{blk}')
+
+for i, epsilon in enumerate(epsilons):
+    succ_adv_folder = f"./data3/{attack}_{epsilon}/{class_name}/{succ_unsucc}/{succ_unsucc}_x/"
+    adv_img = return_a_img(succ_adv_folder, img_num=img_num)
+    adv_attn_maps = get_attention_maps(input_img=adv_img.cuda(), model=model)
+    axes[i, 0].imshow(np.transpose(adv_img.squeeze().cpu().numpy(), (2, 1, 0)))  # Use the 4th row
+
+    for blk in range(12):
+        axes[i, blk+1].imshow(adv_attn_maps[blk], cmap='inferno')
+        axes[i, blk+1].set_title(f'E:{epsilon} B:{blk}')
+
+for ax in axes.flat:
+    ax.axis('off')
+
+plt.subplots_adjust(left=0.02, bottom=0.02, right=0.98, top=0.98, wspace=0.05, hspace=0.05) 
+plt.show()
